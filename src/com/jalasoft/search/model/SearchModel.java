cb0fe74f5a7563c1985c0a0042ff5943ed576dad
Gabriel Rendon <gabriel.rendon@communitybrands.com>
2018-03-21 14:59:45 -0700

    Model with commentaries examples

--- src/com/jalasoft/search/model/SearchModel.java
+++ src/com/jalasoft/search/model/SearchModel.java
@@ -1,14 +1,37 @@
+/*
+ * @(#)Blah.java        1.82 99/03/18
+ *
+ * Copyright (c) 1994-1999 Sun Microsystems, Inc.
+ * 901 San Antonio Road, Palo Alto, California, 94303, U.S.A.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of Sun
+ * Microsystems, Inc. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Sun.
+ */
 package com.jalasoft.search.model;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ *
+ Class description goes here.
+ *
+ * @version
+1.82 18 Mar 1999  * @author
+Firstname Lastname  */
 public class SearchModel {
+    /* A class implementation comment can go here. */
     public void ListFileByPath(String path){ //inputs path, file name, etc
         File files = new File(path);
         File[] listOfFiles = files.listFiles();
         for (File file : listOfFiles){
             if(file.isFile()) {
+                /**
+                 classVar1 documentation comment */
                 //Variables
                 SimpleDateFormat lastModifiedDateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
                 Long space = file.getTotalSpace();
