7c09bba8f224e96ff0459abd0f2d57cd3d58f2bb
GabrielRendon <gabriel.rendon@jalasoft.com>
2018-03-22 21:55:01 +0000

    Controller comments

--- src/com/jalasoft/search/Controller/SearchController.java
+++ src/com/jalasoft/search/Controller/SearchController.java
@@ -1,28 +1,50 @@
+/*
+ * @(#)SearchController.java        1.00 3/22/18
+ *
+ * Copyright (c) Jalasoft
+ *
+ * This software is the confidential and proprietary information of Jalasoft
+ * ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
 package com.jalasoft.search.Controller;
-
+import com.jalasoft.search.model.SearchModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+/**
+ * SearchController
+ *
+ * @version 1.00 23 Mar 2018
+ * @author Gabriel Rendon  */
 public class SearchController {
+    /* Controller. */
+    /**
+     private model, view */
     private SearchModel model;
-    private SearchView view;
+    //private SearchCriteria criteria;
+    //private SearchModel search = new SearchModel(criteria);
+    private MainFileSearch view;
+    //private Validation valid = mew Validation();
+    //search.getResults();
 
-    public SearchController(SearchModel model, SearchView view){
+
+    public SearchController(SearchModel model, MainFileSearch view){
         this.model = model;
         this.view = view;
-
         this.view.addSearchListener(new SearchListener());
     }
 
     public class SearchListener implements ActionListener {
-
-
         public void actionPerformed(ActionEvent action){
             //Log action here
             try{
                 action = view.getCriteria();
+                //criteria.setCriteria(action);
                 model.setCriteria(action);
-                view.search(model.getResult());
+                var results = model.getResult();
+                view.printResults(results);
             }
         }
     }
