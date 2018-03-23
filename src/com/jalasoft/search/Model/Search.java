cbe39f491e50e3d3f589fd063a003caf5c37dd75
WIN-IT92TJKOQE6\Jonas Aramayo <jonas.aramayo@jalasoft.com>
2018-03-23 19:45:03 -0400

    Removed some validations because Gabriel was working on this, and also removed absolute paths and put relative paths.

--- src/com/jalasoft/search/Model/Search.java
+++ src/com/jalasoft/search/Model/Search.java
@@ -20,41 +20,51 @@ import java.io.IOException;
 
 public class Search {
     private String folderPath;
+    private String fileNameToSearch;
+    private Boolean isFound=Boolean.FALSE;
 
-    public Search(String path) {
+
+    public Search(String path, String fileName) {
         this.folderPath = path;
+        this.fileNameToSearch = fileName;
     }
 
     private String getFilePath() {
         return folderPath;
     }
 
-    void doListFiles(){
+    public void doListFiles(){
          //Read file
         File file = new File(folderPath);
 
-        //folderExist
-        if (file.exists() == true) {
-            //isDirectory
-            if (file.isDirectory()==true){
-
-                //doListFiles
-                File list[]=file.listFiles();
-                for(int i=0;i<list.length;i++){
-                    System.out.println(list[i]);
-                }
-            }
-
-        } else {
-            System.out.println("the folder does not exist");
+        //doListFiles
+        File list[]=file.listFiles();
+        for(int i=0;i<list.length;i++){
+                 System.out.println(list[i].getName());
         }
+    }
 
+    public void searchByFileName(){
 
+        File file = new File(folderPath);
+        //SearchIntoFolder
+        File list[]=file.listFiles();
+        for(int j=0;j<list.length;j++){
+            if (list[j].getName().equals(fileNameToSearch)){
+                System.out.println("The file-> "+list[j].getName()+" was found!");
+                isFound = Boolean.TRUE;
+                break;
+            }
+        }
+        if(isFound == Boolean.FALSE){
+            System.out.println("The file->"+ fileNameToSearch +" was not found!");
+        }
     }
 
     //Only for testing purposes
     public  static void main(String args[]){
-        Search search = new Search("C:\\Test");
+        Search search = new Search("\\Test", "test3.txt");
         search.doListFiles();
+        search.searchByFileName();
     }
 }
